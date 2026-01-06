0ef301f1cdb140acfae92e976b650b9625c58faa
ABNECH <mecinfotec@yahoo.com.ar>
2026-01-06 10:56:40 -0300

    fix: use firebase service account from env for render

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
