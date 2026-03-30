26e4f3c57a88a9812d1387f986467725a482b031
ABNECH <mecinfotec@yahoo.com.ar>
2026-03-30 11:40:34 -0300

    fix: restore clubs array + fix banner rendering}

--- frontend/js/clubs.js
+++ frontend/js/clubs.js
@@ -1,20 +1,66 @@
-
-  const clubs = [
-    {
-      name: "UNIÓN",
-      img: "./img/clubs/union.png",
-      link: "#",
-      city: "Resistencia",
-      year: "1998",
-      stadium: "Estadio Unión"
-    },
-    {
-      name: "FUNEBRERO",
-      img: "./img/clubs/funebrero.png",
-      link: "#",
-      city: "Barranqueras",
-      year: "2005",
-      stadium: "Cancha Funebrero"
-    }
-    // seguí con los demás
-  ];
+const clubs = [
+  {
+    name: "UNIÓN",
+    img: "./img/clubs/union.png",
+    link: "#",
+    city: "Resistencia",
+    year: "1998",
+    stadium: "Estadio Unión"
+  },
+  {
+    name: "FUNEBRERO",
+    img: "./img/clubs/funebrero.png",
+    link: "#",
+    city: "Barranqueras",
+    year: "2005",
+    stadium: "Cancha Funebrero"
+  },
+  {
+    name: "CFA",
+    img: "./img/clubs/cfa.png",
+    link: "#",
+    city: "Resistencia",
+    year: "2010",
+    stadium: "Club CFA"
+  },
+  {
+    name: "GENERAL VEDIA",
+    img: "./img/clubs/general-vedia.png",
+    link: "#",
+    city: "Gral. Vedia",
+    year: "2001",
+    stadium: "Estadio Vedia"
+  },
+  {
+    name: "LA LEONESA",
+    img: "./img/clubs/la-leonesa.png",
+    link: "#",
+    city: "La Leonesa",
+    year: "2003",
+    stadium: "Club Leonesa"
+  },
+  {
+    name: "PALERMO CAP",
+    img: "./img/clubs/palermo-cap.png",
+    link: "#",
+    city: "Resistencia",
+    year: "1995",
+    stadium: "Estadio CAP"
+  },
+  {
+    name: "PUERTO BERMEJO",
+    img: "./img/clubs/puerto-bermejo.png",
+    link: "#",
+    city: "Pto. Bermejo",
+    year: "2008",
+    stadium: "Club Bermejo"
+  },
+  {
+    name: "ZAPALLAR",
+    img: "./img/clubs/zapallar.png",
+    link: "#",
+    city: "Zapallar",
+    year: "2012",
+    stadium: "Club Zapallar"
+  }
+];
